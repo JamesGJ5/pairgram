9124bce2970354f332b92b51df19f7adee8be7bc
James Graca-Jones <jamesgracajones@gmail.com>
2024-03-23 21:32:53 +0000

    Make boilerplate privacy policy and ToS

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,5 +1,21 @@
 export default function Page() {
   return (
-    <></>
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque purus semper eget duis at tellus at urna condimentum. Sit amet mauris commodo quis imperdiet massa. Turpis in eu mi bibendum. Pharetra magna ac placerat vestibulum lectus mauris. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Laoreet non curabitur gravida arcu ac tortor. Consequat id porta nibh venenatis cras sed felis. Amet venenatis urna cursus eget nunc scelerisque viverra. Viverra aliquet eget sit amet tellus cras.
+      </p>
+      <p>
+        Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Erat nam at lectus urna duis convallis. Duis tristique sollicitudin nibh sit amet. Sed faucibus turpis in eu. Purus faucibus ornare suspendisse sed. Tellus in hac habitasse platea dictumst. Integer vitae justo eget magna fermentum iaculis eu non. Nisi est sit amet facilisis magna etiam tempor. Volutpat est velit egestas dui id ornare arcu odio ut. Amet aliquam id diam maecenas ultricies mi eget mauris pharetra. Curabitur vitae nunc sed velit dignissim sodales. Orci sagittis eu volutpat odio facilisis mauris sit. Posuere urna nec tincidunt praesent semper feugiat nibh sed. Ut sem viverra aliquet eget sit amet. Orci eu lobortis elementum nibh tellus. Sit amet porttitor eget dolor morbi non arcu. Libero id faucibus nisl tincidunt eget. Aliquam purus sit amet luctus venenatis lectus magna. Risus in hendrerit gravida rutrum quisque non tellus. Venenatis cras sed felis eget velit aliquet sagittis id.
+      </p>
+      <p>
+        Diam vel quam elementum pulvinar etiam. Sit amet nisl purus in mollis. Facilisis volutpat est velit egestas dui id ornare. Adipiscing enim eu turpis egestas pretium aenean pharetra. Arcu dictum varius duis at consectetur lorem donec massa sapien. Bibendum ut tristique et egestas quis ipsum suspendisse. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque volutpat. Facilisis volutpat est velit egestas dui. Mattis rhoncus urna neque viverra.
+      </p>
+      <p>
+        Ultrices neque ornare aenean euismod. Euismod elementum nisi quis eleifend quam. Lorem mollis aliquam ut porttitor leo. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Vestibulum mattis ullamcorper velit sed ullamcorper morbi. Velit laoreet id donec ultrices tincidunt arcu non. Libero justo laoreet sit amet cursus sit. Fusce id velit ut tortor. Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Lorem sed risus ultricies tristique nulla. Arcu vitae elementum curabitur vitae nunc sed velit. Ornare massa eget egestas purus viverra accumsan in nisl. Tortor consequat id porta nibh venenatis. Amet massa vitae tortor condimentum lacinia quis vel. Convallis a cras semper auctor. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Interdum posuere lorem ipsum dolor sit.
+      </p>
+      <p>
+        Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Purus in massa tempor nec feugiat nisl pretium. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Rhoncus mattis rhoncus urna neque viverra. Commodo odio aenean sed adipiscing. Tincidunt dui ut ornare lectus sit amet est placerat in. Pellentesque sit amet porttitor eget dolor morbi non arcu. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.
+      </p>
+    </>
   );
 }
