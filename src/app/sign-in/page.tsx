6499e23347edad2c32194022f3d3f55768c2844c
James Graca-Jones <jamesgracajones@gmail.com>
2024-03-23 21:18:13 +0000

    Make static sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <></>
+    <>
+      <main>
+        <form>
+          <label>
+            *E-mail: <input type="email"/>
+          </label>
+          <label>
+            *Password: <input type="email"/>
+          </label>
+        </form>
+        <a href="/create-account">Create Account</a>
+        <a href="/forgot-password">Forgot Password</a>
+      </main>
+      <footer> {/* TODO: put into layout if to be reused generally*/}
+        <a href="/privacy-policy">Privacy Policy</a>
+        <a href="/terms-of-service">Terms of Service</a>
+      </footer>
+    </>
   );
 }
