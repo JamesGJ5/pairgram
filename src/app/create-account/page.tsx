620a5962709827d8be69e869e868240ad7412002
James Graca-Jones <jamesgracajones@gmail.com>
2024-03-24 17:43:46 +0000

    Make static account creation page

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,5 +1,23 @@
 export default function Page() {
   return (
-    <></>
+    <>
+      <form>
+        <label>
+          *Name: <input type="text"/>
+        </label>
+        <label>
+          *E-mail: <input type="email"/>
+        </label>
+        <label>
+          *Password: <input type="password"/>
+        </label>
+        <label>
+          *Confirm password: <input type="password"/>
+        </label>
+        <label>
+          *Check this box to confirm you've read our <a href="/privacy-policy">Privacy Policy</a> and <a href="/terms-of-service">Terms of Service</a>: <input type="password"/>
+        </label>
+      </form>
+    </>
   );
 }
