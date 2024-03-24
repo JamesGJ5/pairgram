5b25f05e44f1b89922e3b325d0cf5479b37ff719
James Graca-Jones <jamesgracajones@gmail.com>
2024-03-24 18:04:49 +0000

    Require inputs

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -3,20 +3,23 @@ export default function Page() {
     <>
       <form>
         <label>
-          *Name: <input type="text"/>
+          *Name: <input type="text" required/>
         </label>
         <label>
-          *E-mail: <input type="email"/>
+          *E-mail: <input type="email" required/>
         </label>
         <label>
-          *Password: <input type="password"/>
+          *Password: <input type="password" required/>
+        </label>
+        {/* TODO: assert that the passwords are the same */}
+        {/* TODO: asser that the passwords are acceptable */}
+        <label>
+          *Confirm password: <input type="password" required/>
         </label>
         <label>
-          *Confirm password: <input type="password"/>
+          *Check this box to confirm you've read our <a href="/privacy-policy">Privacy Policy</a> and <a href="/terms-of-service">Terms of Service</a>: <input type="checkbox" required/>
         </label>
-        <label>
-          *Check this box to confirm you've read our <a href="/privacy-policy">Privacy Policy</a> and <a href="/terms-of-service">Terms of Service</a>: <input type="checkbox"/>
-        </label>
+        <button type="submit">Submit</button>
       </form>
     </>
   );
